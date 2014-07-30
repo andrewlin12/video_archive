d04ea9e32caffd392cf10e8448a14aef5a756867
Andrew Lin <andrewlin12@gmail.com>
2014-07-30 10:17:53 -0700

    thumbnail and metadata to S3

--- src/github.com/andrewlin12/video_archive/app.go
+++ src/github.com/andrewlin12/video_archive/app.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+  "bufio"
+  "crypto/md5"
   "encoding/json"
   "fmt"
   "html/template"
+  "io"
   "io/ioutil"
   "log"
   "net/http"
@@ -22,6 +25,13 @@ type JsonConfig struct {
   AccessKey string
   SecretKey string
   BucketName string
+}
+
+type VideoMetadata struct {
+  OriginalFileName string
+  Title string
+  Description string
+  Duration float64
 }
 
 var config JsonConfig
@@ -165,7 +175,11 @@ func checkComplete(folderPath string, expectedCount int, filename string) {
     fmt.Printf("Complete file: %s\n", outputPath)
     os.RemoveAll(folderPath)
 
-    basename := path.Base(outputPath)
+    originalBaseName := path.Base(outputPath)
+    md5Hash := md5.New()
+    io.WriteString(md5Hash, fmt.Sprintf("%s|%d", originalBaseName, 
+        time.Now().Unix()))
+    basename := fmt.Sprintf("%x", md5Hash.Sum([]byte{}))
     video1080Path := "/tmp/" + basename + "_1080.mp4"
     video720Path := "/tmp/" + basename + "_720.mp4"
     video360Path := "/tmp/" + basename + "_360.mp4"
@@ -196,14 +210,53 @@ func checkComplete(folderPath string, expectedCount int, filename string) {
     os.RemoveAll(outputPath)
     fmt.Printf("Transcode complete\n")
 
+    // Create a thumbnail
+    thumbPath := "/tmp/" + basename + "_thumb.jpg"
+    cmd = exec.Command("ffmpeg",
+      "-i", video360Path,
+      "-ss", "00:00:01",
+      "-vframes", "1",
+      thumbPath,
+    )
+    stderr, _ := cmd.StderrPipe()
+    scanner := bufio.NewScanner(stderr)
+    err = cmd.Start()
+    duration := 0.0
+    for scanner.Scan() {
+      scanned := scanner.Text()
+      if strings.Index(scanned, "Duration:") != -1 {
+        parts := strings.Split(scanned, ",")
+        parts = strings.Split(parts[0], "Duration: ")
+        parts = strings.Split(parts[1], ":")
+        hours, _ := strconv.ParseFloat(parts[0], 64)
+        hours = hours * 3600
+        minutes, _ := strconv.ParseFloat(parts[1], 64)
+        minutes = minutes * 60
+        seconds, _ := strconv.ParseFloat(parts[2], 64)
+        duration = hours + minutes + seconds
+      }
+    }
+    cmd.Wait()
+    if err != nil {
+      fmt.Printf("Could not transcode file: %v\n", err)
+      return
+    }
+    fmt.Printf("Thumbnail complete: %s\n", thumbPath)
+
     s3Bucket := getS3Bucket()
-    for _, videoPath := range [...]string{ video360Path, video720Path,
-        video1080Path } {
+    for _, videoPath := range [...]string{ thumbPath, video360Path,
+        video720Path, video1080Path } {
       videoStat, _ := os.Stat(videoPath)
-      uploadFilename := strings.Replace(videoPath, "/tmp", "blarg", -1)
+      uploadFilename := strings.Replace(videoPath, "/tmp", basename, -1)
       videoReader, _ := os.Open(videoPath)
+      var contentType string
+      if (path.Ext(videoPath) == ".jpg") {
+        contentType = "image/jpg"
+      } else { 
+        contentType = "video/mp4"
+      }
       err := s3Bucket.PutReader(uploadFilename, videoReader, videoStat.Size(),
-          "video/mp4", s3.PublicRead)
+          contentType, s3.PublicRead)
       if err != nil {
         fmt.Printf("Failed to upload %s: %v\n", videoPath, err)
       } else {
@@ -211,5 +264,14 @@ func checkComplete(folderPath string, expectedCount int, filename string) {
         os.RemoveAll(videoPath)
       }
     }
+    jsonMetadata, _ := json.Marshal(VideoMetadata{
+      originalBaseName,
+      "Untitled",
+      "",
+      duration,
+    })
+    _ = s3Bucket.Put("/" + basename + "/metadata.json", 
+      []byte(jsonMetadata), "text/json", s3.PublicRead)
+    fmt.Printf("Metadata written\n")
   }
 }
